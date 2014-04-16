27e2ea0b4769c817bc4dfe48007b41da4eeab8df
GP <exchequer598@gmail.com>
2014-04-16 19:45:39 +0530

    Major mailer changes - completes email functionality as well.

--- utils/mail.js
+++ utils/mail.js
@@ -10,13 +10,28 @@
  */
 
 var config = require('../config/config'),
-    nodemailer = require('nodemailer');
+    nodemailer = require('nodemailer'),
+    swig = require('swig');
+
+// SMTP transport for debugging. Not sure if I should be distributing this.
+var transport = nodemailer.createTransport('SMTP', {
+    service: 'Gmail',
+    auth: {
+        XOAuth2: {
+            user: 'exchequer598@gmail.com',
+            clientId: '1097162541020.apps.googleusercontent.com',
+            clientSecret: '6ea8NmVKD7zUa6obgc8HCzps',
+            refreshToken: '1/luLonngmJ5wnWvfUYwSSuuTOUlKIsKrTPWTVMk08zbg'
+        }
+    }
+});
 
 // SMTP transport object.
-var transport = nodemailer.createTransport("SMTP", {
+var transport = nodemailer.createTransport('SMTP', {
     host: config.MAIL_HOST,
     port: config.MAIL_PORT,
     secureConnection: config.MAIL_SECURE,
+    ignoreTLS: !config.MAIL_USE_TLS,
     debug: true,
     auth: {
         user: config.MAIL_USERNAME,
@@ -38,15 +53,26 @@ var sendActivationLink = function(domain, username, activate_key, err) {
     var message = {
         from: config.MAIL_SENDER,
         to: username + config.MAIL_USER_DOMAIN,
-        subject: '» Activate your GA Quiz Account',
-        html: '<h1>jhasjd</h1>',
-        text: 'Click to activate this link - ' + domain + '/activate/' + activate_key
+        subject: '» ' + config.APP_TITLE + ' - Activate your Account',
+        attachments: [{
+            filename: 'logo.png',
+            filePath: config.MAIL_LOGO,
+            cid: 'app_logo'
+        }],
+        html: swig.renderFile(config.MAIL_TEMPLATE, {
+            app_title: config.APP_TITLE,
+            main_link: 'http://' + domain,
+            mail_mode: 'activation',
+            activation_link: 'http://' + domain + config.URL.ACTIVATE + '/' + activate_key
+        })
     };
-    console.log(message.text);
     transport.sendMail(message, function(err, response) {
         if (err) {
-            console.log('Email not sent!');
-            console.log(err);
+            logger.log('error', 'CRITICAL ERROR! USERNAME ACTIVATION EMAIL NOT SENT', {
+                'username': message.to,
+                'activate_key': activate_key,
+                'stack': err.stack
+            });
         } else {
             console.log('Message sent!');
         }
@@ -70,16 +96,25 @@ var mailResetKey = function(domain, ip, user_cookie, username, reset_key, err) {
     var message = {
         from: config.MAIL_SENDER,
         to: username + config.MAIL_USER_DOMAIN,
-        subject: '» Your GA Quiz Account\'s New Password',
-        html: 'Your password was reset from IP ' + ip + shame,
-        text: 'Your reset key is - ' + reset_key +
-            '   Click to reset - ' + domain + config.URL.RESET + '/' + reset_key
+        subject: '» ' + config.APP_TITLE + ' - Reset Your Password',
+        attachments: [{
+            filename: 'logo.png',
+            filePath: config.MAIL_LOGO,
+            cid: 'app_logo'
+        }],
+        html: swig.renderFile(config.MAIL_TEMPLATE, {
+            app_title: config.APP_TITLE,
+            main_link: 'http://' + domain,
+            mail_mode: 'reset_password',
+            reset_link: 'http://' + domain + config.URL.RESET + '/' + reset_key,
+            validity_period: config.RESET_VALIDITY,
+            ip_address: ip,
+            shame_name: shame
+        })
     };
-    console.log(message.html);
-    console.log(message.text);
     transport.sendMail(message, function(err, response) {
         if (err) {
-            logger.log('error', 'CRITICAL ERROR! EMAIL NOT SENT', {
+            logger.log('error', 'CRITICAL ERROR! PASSWORD RESET EMAIL NOT SENT', {
                 'username': message.to,
                 'reset_key': reset_key,
                 'ip': ip,
