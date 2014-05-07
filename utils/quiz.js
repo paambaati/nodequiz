a1cf167c5895d64069a89a89b5e9fb0d5b7c3caa
GP <exchequer598@gmail.com>
2014-05-07 20:16:03 +0530

    Removing redundant code.

--- utils/quiz.js
+++ utils/quiz.js
@@ -234,8 +234,6 @@ function getResults(user_id, start_day, fn) {
                 if (item.question.answer == item.choice_id) {
                     correct_answer = true;
                     total_points++;
-                } else {
-                    correct_answer = false;
                 }
                 results[index] = {
                     'question_title': item.question.title,
